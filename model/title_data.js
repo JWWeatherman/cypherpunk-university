dc270e3bfad3800e4e6e8f960106e0e89f2eb01c
JW Weatherman <jw@jwweatherman.com>
2019-01-15 20:50:33 +0000

    hot update

--- model/title_data.js
+++ model/title_data.js
@@ -2,6 +2,6 @@
  * @description - Here is the spot to change the title and subtitle
  */
 const title = 'Cypherpunk University'
-const subTitle = 'Powerful features for startups, enterprises, and everyone in between.'
+const subTitle = 'Powerful for people'
 
 module.exports = {title, subTitle}
