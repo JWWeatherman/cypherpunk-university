799761f5a793a6a26e979e1d2eab7ad7d7b37fc7
Richard Porter <richard@mathbot.com>
2019-01-15 10:57:13 -0500

    documents resource_list and makes it easy to add resources

--- resources/resource_list.resources.js
+++ resources/resource_list.resources.js
@@ -1,37 +1,72 @@
 class Link {
   /**
    * @param display {string}
-   * @param link {string}
+   * @param url
    */
-  constructor ({display, link} = {display: 'READ', link: '#'}) {
+  constructor (display, url) {
     this.display = display
-    this.link = link
+    this.url = url
   }
 }
 
 class Resource {
   /**
-   * @param name {string}
+   * @param header
    * @param body {string}
    * @param button {Link}
    * @param links {[Link]}
    */
-  constructor (name, body, button, links = []) {
-    this.name = name
+  constructor (header, body, button, links = []) {
+    this.name = header
     this.body = body
     this.button = button
     this.links = links
   }
 }
 
-function fakeResourcesGen () {
-  return [
-    new Resource('Some Cool Learning Resource', 'Create personalized notifications and emails, and have them delivered to each user at the ideal time of day.', new Link(), [new Link({display: 'Listen'}), new Link({display: 'Watch'})]),
-    new Resource('Some Cool Learning Resource', 'Millions of users? We\'ve got them all covered. We support most devices and all major SDKs.', new Link(), [new Link({display: 'Listen'}), new Link({display: 'Watch'})]),
-    new Resource('Some Cool Learning Resource', 'Monitor the conversion of your notifications and emails in real time.', new Link(), [new Link({display: 'Watch'})])
+/**
+ * @type {Resource[]}
+ */
+const exampleResources = [
+    /**
+     * 2 links
+     */
+    new Resource(
+      'Some Cool Learning Resource', // Header
+      'Create personalized notifications and emails, and have them delivered to each user at the ideal time of day.', // Body
+      new Link('READ', '#'), // Button
+      [new Link('Listen', '#'), new Link('Watch', '#')] // Links
+    ),
+    /**
+     * 1 link
+     */
+    new Resource(
+      'Some Cool Learning Resource', // Header
+      'Millions of users? We\'ve got them all covered. We support most devices and all major SDKs.', // Body
+      new Link('READ', '#'), // Button
+      [new Link('Listen', '#')] // Links
+    ),
+    /**
+     * no links
+     * notice links property is omitted, the is because there is a default value set of an empty array.
+     */
+    new Resource(
+      'Some Cool Learning Resource', // Header
+      'Millions of users? We\'ve got them all covered. We support most devices and all major SDKs.', // Body
+      new Link('READ', '#') // Button
+    )
   ]
-}
 
-const resources = fakeResourcesGen()
+/**
+ * @description This is the spot to place actual resources.
+ * When this array is populated it will be rendered automatically.
+ * @type {Resource[]}
+ */
+const resources = [
+  // ADD ACTUAL RESOURCES HERE
+]
 
-module.exports = resources
+/**
+ * @type {Resource[]}
+ */
+module.exports = resources.length ? resources : exampleResources
