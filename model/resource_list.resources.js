ff8b9071e23f8c1f8e61ff6c0e7be285cc31583c
JW Weatherman <jw@jwweatherman.com>
2019-01-26 00:39:18 +0000

    hot update

--- model/resource_list.resources.js
+++ model/resource_list.resources.js
@@ -44,7 +44,7 @@ class Resource {
 const resources = [
   new Resource(
     '10 minutes - Bitcoin Standard Prologue', // Header
-    'Get an introduction and overview of Bitcoin from Saifedean Ammous the author of The Bitcoin Standard.', // Body
+    'Get an excellent introduction and overview of Bitcoin from Saifedean Ammous the author of indispensable book The Bitcoin Standard.', // Body
     new Link('LISTEN', 'https://bitcoinstandardintro.podbean.com/e/prologue-of-the-bitcoin-standard/'), // Button
     [
       new Link('Read', 'https://docs.google.com/document/d/1AOUazAHeG_49s53eJifosdY9cFVvA7ZZOhICwebAgws/edit?usp=sharing'),
@@ -57,12 +57,12 @@ const resources = [
   ),
   new Resource(
     '2 hour - Bitcoin Mechanics', // Header
-    'A highlevel overview of the bitcoin software and network.', // Body
+    'A high-level overview of the bitcoin software and network that is appropriate for someone that is not a software developer.', // Body
     new Link('Read', 'http://unenumerated.blogspot.com/2018/03/the-many-traditions-of-non-governmental.html') // Bu#tton
   ),
   new Resource(
     '1 hour - Shelling Out: The Origins of Money', // Header
-    'The precursors of money, along with language, enabled early modern humans to solve problems of cooperation that other animals cannot – including problems of reciprocal altruism, kin altruism, and the mitigation of aggression. These precursors shared with non-fiat currencies very specific characteristics – they were not merely symbolic or decorative objects.', // Body
+    'This excellent paper written by Nick Szabo is widely regarded as one of the best resources to understand money and the role bitcoin hopes to play globally', // Body
     new Link('Read', 'http://unenumerated.blogspot.com/2018/03/the-many-traditions-of-non-governmental.html') // Bu#tton
   ),
   new Resource(
